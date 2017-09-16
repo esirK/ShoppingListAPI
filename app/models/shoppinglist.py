c1d31173d81aba09ea5d0806d974d100c26c772f
esirK <esirkings@gmail.com>
2017-09-16 06:28:55 +0300

    Create a one to Many Relationship

--- app/models/shoppinglist.py
+++ app/models/shoppinglist.py
@@ -6,3 +6,9 @@ class ShoppingList(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
     description = db.Column(db.String(180))
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __init__(self, name, description, owner):
+        self.name = name
+        self.description = description
+        self.owner_id = owner.id
--- app/models/user.py
+++ app/models/user.py
@@ -1,20 +1,20 @@
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from app import db
 
 
 class User(db.Model):
-    __tablename__ = "users"
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64))
     email = db.Column(db.String(64), unique=True, index=True)
-    password = db.Column(db.String(64))
+    password = db.Column(db.String(104))
+    shopping_lists = db.relationship('ShoppingList', backref='owner', lazy='dynamic')
 
     def __init__(self, username, email, password):
         self.username = username
         self.email = email
-        self.password = generate_password_hash(password)
-        self.shopping_lists = {}
+        self.set_password(password)
 
     def __repr__(self):
         """
@@ -22,3 +22,12 @@ class User(db.Model):
          and testing purposes. 
         """
         return '<User %r>' % self.email
+
+    def set_password(self, password):
+        """
+        Sets current users password as a password hash
+        """
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
