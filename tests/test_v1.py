8aa458444e8f007a9935092e9c68201eb8528a4a
esirK <esirkings@gmail.com>
2017-09-24 11:27:33 +0300

    Fix Failing tests

--- tests/test_v1.py
+++ tests/test_v1.py
@@ -7,35 +7,36 @@ from app import create_app, db
 
 
 class TestMain(unittest.TestCase):
+    configurations = "testing"
+    app = create_app(configurations)
+    app.app_context().push()
+    db.drop_all()
+    db.create_all()
+
     def setUp(self):
-        self.configurations = "testing"
-        self.app = create_app(self.configurations)
-        with self.app.app_context():
-            self.client = self.app.test_client()  # create all tables
-            db.drop_all()
-            db.create_all()
-            self.headers = {
-                'Authorization': 'Basic %s' %
-                                 b64encode(b"tinyrick@gmail.com:python")
-                                     .decode("ascii")
-            }
-            self.client.post("/api/register",
-                             data=json.dumps({"name": "tinyrick",
-                                              "password": "python",
-                                              "email": "tinyrick@gmail.com"
-                                              }),
-                             content_type='application/json')
-            res = self.client.get("/api/token", headers=self.headers)
-            token = json.loads(res.data)['token']
-            self.token_headers = {
-                'Authorization': 'Basic %s'
-                                 % b64encode(bytes(token + ':', "utf-8"))
-                                     .decode("ascii")
-            }
+        self.client = self.app.test_client()  # create all tables
+        self.headers = {
+            'Authorization': 'Basic %s' %
+                             b64encode(b"tinyrick@gmail.com:python")
+                                 .decode("ascii")
+        }
+        self.client.post("/v_1/register",
+                         data=json.dumps({"name": "tinyrick",
+                                          "password": "python",
+                                          "email": "tinyrick@gmail.com"
+                                          }),
+                         content_type='application/json')
+        res = self.client.get("/v_1/token", headers=self.headers)
+        token = json.loads(res.data)['token']
+        self.token_headers = {
+            'Authorization': 'Basic %s'
+                             % b64encode(bytes(token + ':', "utf-8"))
+                                 .decode("ascii")
+        }
 
     def test_user_registration(self):
         """Test if a user is created using the api"""
-        response = self.client.post("/api/register",
+        response = self.client.post("/v_1/register",
                                     data=json.dumps({"name": "esirick",
                                                      "password": "morty",
                                                      "email": "esimorty@gmail.com"
@@ -44,13 +45,13 @@ class TestMain(unittest.TestCase):
         self.assertEqual(response.status_code, 201)
         self.assertIn(
             "Already Exists",
-            self.client.post("/api/register",
+            self.client.post("/v_1/register",
                              data=json.dumps({"name": "esirick",
                                               "password": "python",
                                               "email": "esimorty@gmail.com"
                                               }),
                              content_type='application/json').data.decode())
-        response_no_arg = self.client.post("/api/register",
+        response_no_arg = self.client.post("/v_1/register",
                                            data=json.dumps({"name": "esirick",
                                                             "password": "python",
                                                             "email": "  "
@@ -65,7 +66,7 @@ class TestMain(unittest.TestCase):
         """
         Test if user is authenticated using email and password
         """
-        res = self.client.get("/api/token", headers=self.headers)
+        res = self.client.get("/v_1/token", headers=self.headers)
         self.assertEqual(res.status_code, 200)
         self.assertIn("token", res.data.decode())
 
@@ -73,7 +74,7 @@ class TestMain(unittest.TestCase):
         """
         Test if user is authenticated using a valid token
         """
-        res = self.client.get("/api/token", headers=self.token_headers)
+        res = self.client.get("/v_1/token", headers=self.token_headers)
         """Accessing Secure endpoint using a token"""
         self.assertEqual(res.status_code, 200)
         self.assertIn("token", res.data.decode())
