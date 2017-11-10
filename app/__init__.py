9195f7d388145972a310e7dfd34a5f3df54514e5
esirK <esirkings@gmail.com>
2017-11-10 15:35:33 +0300

    Add flask-cors for for sending Access-Control-Allow-Origin and other headers

--- app/__init__.py
+++ app/__init__.py
@@ -6,11 +6,14 @@ from app.configurations import config
 from app.apis.v1 import api, bp, ns
 from app.init_db import db
 
+from flask_cors import CORS
+
 api.add_namespace(ns, path="/v1")
 
 
 def create_app(config_name):
     app = Flask(__name__)
+    CORS(app)
     Migrate(app, db)
     app.config.from_object(config[config_name])
     app.register_blueprint(bp)
